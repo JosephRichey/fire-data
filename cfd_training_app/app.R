b742ee51ac2b931681c59484fd0d922a255965ee
JosephRichey <jwrichey@gmail.com>
2023-11-13 21:01:06 -0700

    Added all modification and deletion of trainings. There is a bug where trainings aren't deleted properly that needs to be addressed.

--- cfd_training_app/app.R
+++ cfd_training_app/app.R
@@ -4,6 +4,7 @@ library(bslib)
 library(pins)
 require(googledrive)
 library(data.table)
+library(DT)
 library(dplyr)
 
 # Set options to auth google drive
@@ -14,13 +15,14 @@ options(
     gargle_oauth_cache = "cfd_training_app/.secrets"
 )
 
-if (getwd() == "C:/Users/jwric/OneDrive/Documents/cfd_training_app/cfd_training_app") {
+if (getwd() == "C:/Users/jwric/OneDrive/Documents/cfd_training_app") {
     googledrive::drive_auth(token = readRDS(here::here("cfd_training_app/.secrets/c13dc354db9600c8cd9b2bd868d1bf25_corinne.fire.data@gmail.com")))
 } else {
     googledrive::drive_auth(token = readRDS(here::here(".secrets/c13dc354db9600c8cd9b2bd868d1bf25_corinne.fire.data@gmail.com")))
 }
 
 board <- board_gdrive("CFD Training App")
+Trainings <- board |> pin_read("trainings")
 
 FixColNames <- function(Data) {
     colnames(Data) <- gsub("_", " ", colnames(Data))
@@ -29,7 +31,11 @@ FixColNames <- function(Data) {
     return(Data)
 }
 
-
+load_data <- function() {
+  Sys.sleep(2)
+  hide("loading_page")
+  show("main_content")
+}
 
 ui <- page_navbar(
     title = "Corinne Fire Department",
@@ -39,18 +45,36 @@ ui <- page_navbar(
     nav_panel(title = "Add Training", 
         layout_sidebar(
             sidebar = sidebar(
+                width = 400,
+                open = "desktop",
                 actionButton('add_training', "Add Training"),
-                dateRangeInput('training_filter_range',
-                               "Show trainings between:",
-                               start = Sys.Date() - 365,
-                               end = Sys.Date() + 30)
+                actionButton('modify_training', "Modify Training"),
+                actionButton("delete_training", "Delete Training"),
+                accordion(
+                  open = FALSE,
+                  accordion_panel(
+                    title = "Filter Trainings",
+                    open = FALSE,
+                    card(
+                      helpText("Filter by training date"),
+                      dateRangeInput('training_filter_range',
+                                     "Show trainings between:",
+                                     start = Sys.Date() - 365,
+                                     end = Sys.Date() + 30),
+                      helpText("Filter by training type"),
+                      pickerInput('filter_training_type',
+                                  'Training Type',
+                                  choices = unique(Trainings$training_type),
+                                  selected = unique(Trainings$training_type),
+                                  options = list(`actions-box` = TRUE),
+                                  multiple = TRUE
+                      )
+                    )
+                  )
+                )
             ),
-            card(dataTableOutput('view_trainings'))
+            card(DTOutput('view_trainings'))
         )
-    ),
-    
-    nav_panel(title = "Manage Training", 
-        layout_sidebar()
     ),
     
     nav_panel(title = "Manage Roster", 
@@ -58,7 +82,7 @@ ui <- page_navbar(
             sidebar = sidebar(actionButton('add_firefighter', 'Add Firefighter'),
                               actionButton('remove_firefighter', 'Remove Firefighter')
                               ),
-            card(dataTableOutput('roster')
+            card(DTOutput('roster')
             )
         ),
             
@@ -76,6 +100,7 @@ ui <- page_navbar(
 
 
 server <- function(input, output, session) {
+  load_data()
     
     # Use reactiveValues to maintain a local copy of the roster that is available at all times.
     # Update the local copy whenever the stored copy is updated.
@@ -109,20 +134,23 @@ server <- function(input, output, session) {
     
     ###### Add Trainings #####
     # Display current trainings
-    output$view_trainings <- renderDataTable({
+    output$view_trainings <- renderDT({
         # browser()
         Table_Data <- MyReactives$trainings |>
             filter(date > input$training_filter_range[1] &
-                   date < input$training_filter_range[2]) |>
-            select(-training_id)
+                   date < input$training_filter_range[2] &
+                   training_type %in% input$filter_training_type &
+                   delete == FALSE) |>
+            select(-training_id, -delete)
         
         Table_Data <- FixColNames(Table_Data)
         
-        data.table(Table_Data)
+        DT::datatable(Table_Data,
+                      selection = 'single')
         
     })
     
-    # Update the training topic based on the training type.
+    # Update the add training topic based on the training type.
     observe({
         # browser()
         x <- input$add_training_type
@@ -156,13 +184,51 @@ server <- function(input, output, session) {
         }
     })
     
+    # Update the modify training topic based on the type input
+    observe({
+      # browser()
+      x <- input$modify_training_type
+      
+      if(is.null(x)) {
+        # Do nothing
+      }
+      
+      # Can also set the label and select items
+      else if(x == "EMS") {
+        updateSelectInput(session,
+                          "modify_training_topic",
+                          choices = c(
+                            "Airway/Respiraroty/Ventilation",
+                            "Cardiovascular",
+                            "Trauma",
+                            "Medical",
+                            "Operations"
+                          ),
+                          selected = MyReactives$trainings[input$view_trainings_cell_clicked$row,]$topic
+                          )
+      }
+      
+      else if(x == "Fire") {
+        updateSelectInput(session,
+                          "modify_training_topic",
+                          choices = c(
+                            "Fire 1",
+                            "Fire 2",
+                            "Hazmat",
+                            "Ops"
+                          ),
+                          selected = MyReactives$trainings[input$view_trainings_cell_clicked$row,]$topic
+                          )
+      }
+    })
+    
     # Enter information to create the training.
     observeEvent(input$add_training, {
         showModal(modalDialog(
             selectInput('add_training_type', 'Training Type', choices = c("EMS", "Fire", "Wildland"), selected = "EMS"),
             selectInput('add_training_topic', 'Training Topic', choices = c()), # Update with above observe statement
             numericInput('add_training_length', "Training Length (Hours)", value = 2),
-            textInput('add_description', 'Training Description'),
+            textAreaInput('add_description', 'Training Description'),
             dateInput('add_training_date', 'Training Date', value = Sys.Date()),
             title = "Add Training",
             footer = tagList(
@@ -199,8 +265,111 @@ server <- function(input, output, session) {
         
     })
     
+    # Modify the training
+    observeEvent(input$modify_training, {
+      # browser()
+      cell_click <- input$view_trainings_cell_clicked
+      # Make sure a row was clicked
+      if (length(cell_click) != 0) {
+        showModal(modalDialog(
+          selectInput('modify_training_type', 'Training Type', choices = c("EMS", "Fire", "Wildland"), selected = MyReactives$trainings[cell_click$row,]$training_type),
+          selectInput('modify_training_topic', 'Training Topic', choices = c()),
+          numericInput('modify_training_length', "Training Length (Hours)", value = MyReactives$trainings[cell_click$row,]$training_length),
+          textAreaInput('modify_description', 'Training Description', value = MyReactives$trainings[cell_click$row,]$description),
+          dateInput('modify_training_date', 'Training Date', value = MyReactives$trainings[cell_click$row,]$date),
+          title = "Modify Training",
+          footer = tagList(
+            actionButton("action_modify_training", "Modify Training")
+          ),
+          easyClose = TRUE
+        ))
+      }
+    })
+    
+    
+    observeEvent(input$action_modify_training, {
+      # browser()
+      removeModal()
+      
+      Trainings <- MyReactives$trainings
+      showModal(modalDialog("Please wait...", title = "Processing Changes"))
+      
+      cell_click <- input$view_trainings_cell_clicked
+      MyReactives$trainings <- rows_update(x = Trainings, 
+                                           y = data.frame(training_id = MyReactives$trainings[cell_click$row,]$training_id,
+                                                          training_type = input$modify_training_type,
+                                                          topic = input$modify_training_topic,
+                                                          training_length = input$modify_training_length,
+                                                          description = input$modify_description,
+                                                          date = input$modify_training_date,
+                                                          delete = FALSE
+                                           ),
+                                           by = 'training_id'
+      )
+      
+      board %>% pin_write(MyReactives$trainings, "trainings", "rds")
+      removeModal()
+      showModal(modalDialog("Your training has been successfully modified.",
+                            title = "Success!",
+                            easyClose = TRUE))
+    })
+    
+    observeEvent(input$delete_training, {
+      # browser()
+      cell_click <- input$view_trainings_cell_clicked
+      
+      if (length(cell_click) != 0) {
+        showModal(modalDialog(
+          title = "Confirm Deletion",
+          "Are you sure you want to delete this training? If you're sure, please type \"Confirm Deletion\"",
+          textInput("confirm_deletion", ""),
+          footer = tagList(
+            actionButton("action_delete_training", "Delete Training")
+          ),
+          easyClose = TRUE
+        ))
+      }
+    })
+    
+    observeEvent(input$action_delete_training, {
+      browser()
+      removeModal()
+      
+      if(input$confirm_deletion == "Confirm Deletion") {
+        showModal(modalDialog("Please wait...", title = "Processing Changes"))
+        cell_click <- input$view_trainings_cell_clicked
+        
+        MyReactives$trainings <- rows_update(x = Trainings, 
+                                             y = data.frame(training_id = MyReactives$trainings[cell_click$row,]$training_id,
+                                                            training_type = MyReactives$trainings[cell_click$row,]$training_type,
+                                                            topic = MyReactives$trainings[cell_click$row,]$topic,
+                                                            training_length = MyReactives$trainings[cell_click$row,]$training_length,
+                                                            description = MyReactives$trainings[cell_click$row,]$description,
+                                                            date = MyReactives$trainings[cell_click$row,]$date,
+                                                            delete = TRUE
+                                             ),
+                                             by = 'training_id'
+        )
+        
+        board %>% pin_write(MyReactives$trainings, "trainings", "rds")
+        removeModal()
+        showModal(modalDialog("Your training has been successfully deleted.",
+                              title = "Success!",
+                              easyClose = TRUE))
+        
+      } else {
+        showModal(modalDialog("Records will not be deleted.", title = "Delete Failed"))
+      }
+      Trainings <- MyReactives$trainings
+      
+      
+      
+      
+    })
+    
+    
     ###### Manage Roster ######
-    output$roster <- renderDataTable({
+    output$roster <- renderDT({
         # browser()
         Table_Data <- MyReactives$roster |>
             filter(active_status == TRUE) |>
