775c7f15e79349f456376cbf71fe619b57fbc302
JosephRichey <jwrichey@gmail.com>
2024-01-02 22:04:40 -0700

    Working through database integration. Stuck on error while writing to table.

--- cfd_training_attendance/app.R
+++ cfd_training_attendance/app.R
@@ -5,36 +5,27 @@ library(pins)
 require(googledrive)
 library(data.table)
 library(dplyr)
+library(odbc)
 
-# Set options to auth google drive
-options(
-    # whenever there is one account token found, use the cached token
-    gargle_oauth_email = "corinne.fire.data@gmail.com",
-    # specify auth tokens should be stored in a hidden directory ".secrets"
-    gargle_oauth_cache = "cfd_training_app/.secrets"
-)
+CON = dbConnect(RMySQL::MySQL(),
+                dbname = "cfddb",
+                host = Sys.getenv("CFDDB_HOST"),
+                port = 3306,
+                user = "admin",
+                password = Sys.getenv("CFDDB_PASSWORD"))
 
-if (getwd() == "C:/Users/jwric/OneDrive/Documents/cfd-training-attendance") {
-    googledrive::drive_auth(token = readRDS(here::here(".secrets/c13dc354db9600c8cd9b2bd868d1bf25_corinne.fire.data@gmail.com")))
-} else {
-    googledrive::drive_auth(token = readRDS(here::here(".secrets/c13dc354db9600c8cd9b2bd868d1bf25_corinne.fire.data@gmail.com")))
-}
+Training <- dbGetQuery(CON,
+                       "SELECT * 
+                       FROM cfddb.training
+                       WHERE training_delete IS NULL")
 
-board <- board_gdrive("CFD Training App")
+Roster <- dbGetQuery(CON,
+                     "SELECT * FROM cfddb.firefighter")
 
-Trainings <- board |> pin_read("trainings")  |>  
-    dplyr::mutate(date = as.Date(date)) |> 
-    dplyr::filter(date <= Sys.Date()) |> 
-    arrange(desc(date)) |> 
-    dplyr::mutate(full_desc = paste0(date, ": ", description))
-
-Roster <- board |> pin_read("roster") |> 
-    dplyr::mutate(start_date = as.Date(start_date)) |> 
-    dplyr::filter(active_status == TRUE) |> 
-    dplyr::mutate(full_name = paste(first_name, last_name))
-
-Attendance <- board |> 
-  pin_read("attendance")
+Attendance <- dbGetQuery(CON,
+                         "SELECT * FROM cfddb.attendance") |> 
+  mutate(check_in = as.POSIXct(check_in),
+         check_out = as.POSIXct(check_out))
 
 FixColNames <- function(Data) {
     colnames(Data) <- gsub("_", " ", colnames(Data))
@@ -45,13 +36,13 @@ FixColNames <- function(Data) {
 
 ui <- page_fillable(
     title = "Corinne Fire Department",
+    h1("Corinne Fire Department"),
     theme = bs_theme(version = 5,
                      success = "#375A7F",
                      bootswatch = "darkly"),
     card(
-        selectInput('name', "Name", Roster$full_name, selected = NULL),
-        selectInput('training', "Training", Trainings$full_desc, selected = NULL),
-        actionButton('submit', "Submit")
+        selectInput('name', "Name", Roster$firefighter_full_name, selected = NULL),
+        actionButton('check_in', "Check In")
     )
     
 )
@@ -59,33 +50,61 @@ ui <- page_fillable(
 
 server <- function(input, output, session) {
     
-    observeEvent(input$submit, {
-        showModal(modalDialog("Please wait...", title = "Processing Changes"))
-        
-        target_ff_id <- Roster |> 
-            filter(full_name == input$name) |> 
+    observeEvent(input$check_in, {
+      
+        if(!any(Training$training_date == Sys.Date(), na.rm = TRUE)) {
+          showModal(modalDialog(paste0("There is currently no scheduled training for ",
+                                       Sys.Date(),
+                                       ". It may not have been added to the system yet. ",
+                                       "For further questions please contact Joseph Richey.", 
+                                       "801-644-6893"),
+                                title = "No Valid Training Found"))
+        } else {
+          showModal(modalDialog("Please wait...", title = "Processing Changes"))
+          
+          target_ff_id <- Roster |> 
+            filter(firefighter_full_name == input$name) |> 
             dplyr::select(firefighter_id) |> 
             unlist() |> 
             unname()
-        
-        target_training_id <- Trainings |> 
-            filter(full_desc == input$training) |> 
+          
+          target_training_id <- Training |> 
+            filter(training_date == Sys.Date()) |> 
             dplyr::select(training_id) |> 
             unlist() |> 
             unname()
-        
-        Temp <- data.frame(firefighter_id = target_ff_id,
-          training_id = target_training_id)
-        
-        Attendance <- dplyr::bind_rows(Attendance, Temp)
-        
-        board |> pin_write(Attendance, "attendance", "rds")
-        
-        removeModal()
-        showModal(modalDialog("Your attendace has been successfully added.",
-                              title = "Success!",
-                              easyClose = TRUE))
-        
+          
+          Temp <- data.frame(attendance_id = nrow(Attendance) + 1,
+                             firefighter_id = target_ff_id,
+                             training_id = target_training_id,
+                             check_in = Sys.time())
+                      
+          
+          Attendance <- dplyr::bind_rows(Attendance, Temp)
+          
+          Attendance <- Attendance |> mutate(check_in = as.character(check_in),
+                                             check_out = as.character(check_out))
+          
+          Attendance <- Attendance |> mutate(check_in = as.POSIXct(check_in),
+                                             check_out = as.POSIXct(check_out))
+          
+          dbWriteTable(CON,
+                       name = "cfddb.attendance",
+                       value = Attendance,
+                       field.type = c(attendance_id = "int",
+                                      firefighter_id = "int",
+                                      training_id = "int"),
+                       overwrite = TRUE)
+          
+          dbWriteTable(CON,
+                       "test_table",
+                       value = cars,
+                       row.names = FALSE,
+                       overwrite = TRUE)
+          
+          removeModal()
+          showModal(modalDialog("You have been successfully checked in!", title = "Success!"))
+        }
         
     })
    
