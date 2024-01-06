dd336379a0002d2876ccde71549bee5273546158
JosephRichey <jwrichey@gmail.com>
2024-01-05 21:38:50 -0700

    Refresh button now works. Can't get the table to scroll for some reason.

--- app/view/current_status.R
+++ app/view/current_status.R
@@ -29,10 +29,24 @@ Server <- function(id) {
     id,
     function(input, output, session) {
       
+      #https://stackoverflow.com/questions/61407870/shiny-how-to-refresh-data-loaded-before-server-function
+      rv <- reactiveVal(app_data$Attendance)
+      
       output$current_status <- DT::renderDataTable({
-        
-        app_data$Attendance |> 
-          DT::datatable()
+        # browser()
+        DT::datatable(rv(),
+                      options = list(scrollX=TRUE, scrollCollapse=TRUE))
+      })
+      
+      observeEvent(input$refresh, {
+        # browser()
+        print("Refresh")
+        updated_attendance <- dbGetQuery(app_data$CON,
+                                         "SELECT * FROM cfddb.attendance") |> 
+          mutate(check_in = as.POSIXct(check_in),
+                 check_out = as.POSIXct(check_out))
+          
+        rv(updated_attendance)
       })
       
       
