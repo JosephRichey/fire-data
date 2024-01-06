992296c007bdb15dbb4ab2e5ec2a6ea7dd642dca
JosephRichey <jwrichey@gmail.com>
2024-01-05 23:33:42 -0700

    Updated spacing of code. Made warning messages more specific. Temporarily removed same date filter. Not working properly.

--- app/logic/functions.R
+++ app/logic/functions.R
@@ -11,30 +11,35 @@ box::use(
 #' @export
 VerifyTrainingTime <- function(sysTime) {
   # browser()
+  # FIXME For testing purposes only. Remove before production.
   sysTime <- as.POSIXct("2024-01-03 20:00:13 MST")
   
   today_training <- app_data$Training |> 
     dplyr::filter(training_date == as.Date(sysTime))
   
   if(nrow(today_training) == 0) {
+    
     showModal(modals$noTrainingModal())
-    print("no training modal")
     return(FALSE)
+    
   } else if (nrow(today_training) != 1) {
-    print("Error: More than one training scheduled for a day.")
+    
     showModal(modals$errorModal("Error: More than one training scheduled for a day."))
     return(FALSE)
+    
   }
   
   if(format(sysTime, format = "%H:%M:%S") < "17:55:00" |
      format(sysTime, format = "%H:%M:%S") > "20:05:00") {
+    
     showModal(modals$warningModal("You are outside of applicable check in time. Please wait until 17:55 to check in.
-                        If you forgot to check out, you have been automatically checked out at 20:05."))
-    print("Outside time modal.")
+                        If you forgot to check out, you will be automatically checked out at 20:05."))
     return(FALSE)
+    
   } else {
-    print("Valid check in.")
+    
     return(TRUE)
+    
   }
   
   
@@ -52,9 +57,10 @@ FixColNames <- function(Data) {
 CurrentStatusTable <- function(Attendance, Roster) {
   Attendance |> 
     left_join(Roster) |> 
+    dplyr::filter(is.na(check_out)) |> 
+    # dplyr::filter(as.Date(check_in) == as.Date(Sys.Date())) |> 
     transmute(firefighter_full_name = firefighter_full_name, 
               check_in = format(check_in, "%H:%M:%S"), 
               check_out = format(check_out, "%H:%M:%S")) |> 
-    dplyr::filter(is.na(check_out)) |> 
     FixColNames()
 }
