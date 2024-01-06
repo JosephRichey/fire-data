58aebe71b1c3d1b9575cd73717748f0800e8cf68
JosephRichey <jwrichey@gmail.com>
2024-01-05 23:36:12 -0700

    check_in_out observe event now checks what is in the database at that moment, not at load time. Changed rv to rvs for clarity. Added to warning messages. Had cancel modals reference modals/modals.R Updated read/write to database to fix bugs.

--- app/view/check_in_out.R
+++ app/view/check_in_out.R
@@ -1,6 +1,7 @@
 box::use(
   shiny[...],
   dplyr[filter, ...],
+  DBI[...],
 )
 
 box::use(
@@ -24,66 +25,79 @@ Server <- function(id) {
     id,
     function(input, output, session) {
       
-      rv <- reactiveValues()
+      rvs <- reactiveValues()
+      rv <- reactiveVal(app_data$Attendance)
       
       observeEvent(input$check_in_out, {
         # browser()
-        rv$All <- app_data$Attendance
         
         # Catch all time errors for checking in and display modals.
         # If no errors found, returns true and check in process begins.
         if(functions$VerifyTrainingTime(sysTime = Sys.time()))
         {
           
+          # Update Attendance.
+          
+          Updated_Attendance <- dbGetQuery(app_data$CON,
+                                           "SELECT * FROM cfddb.attendance") |> 
+            mutate(check_in = as.POSIXct(check_in),
+                   check_out = as.POSIXct(check_out))
+          
+          rv(Updated_Attendance)
+          
           # Get ff id and traing id.
           
-          rv$target_ff_id <- app_data$Roster |>
+          rvs$target_ff_id <- app_data$Roster |>
             filter(firefighter_full_name == input$name) |>
             dplyr::select(firefighter_id) |>
             unlist() |>
             unname()
           
-          rv$target_ff_full_name <- app_data$Roster[app_data$Roster$firefighter_id == rv$target_ff_id,]$firefighter_full_name
+          rvs$target_ff_full_name <- app_data$Roster[app_data$Roster$firefighter_id == rvs$target_ff_id,]$firefighter_full_name
           
-          rv$target_training_id <- app_data$Training |>
+          rvs$target_training_id <- app_data$Training |>
             filter(training_date == Sys.Date()) |>
             dplyr::select(training_id) |>
             unlist() |>
             unname()
           
           
-          Firefighter_Attendance <- app_data$Attendance |> 
-            dplyr::filter(firefighter_id == rv$target_ff_id) |> 
-            dplyr::filter(training_id == rv$target_training_id)
+          Firefighter_Attendance <- rv() |> 
+            dplyr::filter(firefighter_id == rvs$target_ff_id) |> 
+            dplyr::filter(training_id == rvs$target_training_id)
           
-          rv$attendance_id <- Firefighter_Attendance$attendance_id
+          rvs$attendance_id <- Firefighter_Attendance$attendance_id
           
           # https://stackoverflow.com/questions/48127459/using-modal-window-in-shiny-module
           ns <- session$ns
           
+          # No check ins today.
           if(nrow(Firefighter_Attendance) == 0) {
             showModal(
               modalDialog(
                 title = "Confirm Check In",
-                paste0(rv$target_ff_full_name, " is not checked in. Would you like to confirm check in?"),
+                paste0(rvs$target_ff_full_name, " is not checked in. Would you like to confirm check in?"),
                 footer = tagList(
                   actionButton(ns("confirm_check_in"), "Confirm Check In"),
                   actionButton(ns("cancel_check_in"), "Cancel")
                 )
               )
             )
+            
+            # They've checked in and out already.
           } else if (nrow(Firefighter_Attendance == 1) & !is.na(Firefighter_Attendance$check_out)) {
             showModal(
               modals$warningModal(
-                paste0(rv$target_ff_full_name, " is already checked out. No changes will be made.")
+                paste0(rvs$target_ff_full_name, " is already checked out from today's training. No changes will be made.")
               )
             )
+            # They've checked in but haven't checked out.
           } else if (nrow(Firefighter_Attendance == 1) & is.na(Firefighter_Attendance$check_out)) {
             # browser()
             showModal(
               modalDialog(
                 title = "Confirm Check Out",
-                paste0(rv$target_ff_full_name, " is checked in. Would you like to confirm check out?"),
+                paste0(rvs$target_ff_full_name, " is checked in. Would you like to confirm check out?"),
                 footer = tagList(
                   actionButton(ns("confirm_check_out"), "Confirm Check Out"),
                   actionButton(ns("cancel_check_out"), "Cancel")
@@ -99,74 +113,69 @@ Server <- function(id) {
       observeEvent(input$cancel_check_in, {
         removeModal()
         showModal(
-          modalDialog(
-            title = "Action canceled.",
-            "No action is taken. Please close this message to return to the app.",
-            easyClose = TRUE
-          )
+          modals$cancelModal()
         )
       })
       
       observeEvent(input$cancel_check_out, {
         removeModal()
         showModal(
-          modalDialog(
-            title = "Action canceled.",
-            "No action is taken. Please close this message to return to the app.",
-            easyClose = TRUE
-          )
+          modals$cancelModal()
         )
       })
       
       observeEvent(input$confirm_check_in, {
         # browser()
         removeModal()
+        sql_command <- paste0(
+          "INSERT INTO cfddb.attendance (firefighter_id, training_id, check_in, check_out) VALUES (",
+           rvs$target_ff_id, ", ",
+           rvs$target_training_id, ", '",
+           as.POSIXct(Sys.time()), "', ",
+           "NULL)")
         
-        Temp <- data.frame(attendance_id = nrow(app_data$Attendance) + 1,
-                           firefighter_id = rv$target_ff_id,
-                           training_id = rv$target_training_id,
-                           check_in = as.POSIXct(Sys.time()))
-        
-        Write_Df <- dplyr::bind_rows(app_data$Attendance, Temp)
-        rv$All <- Write_Df
-        
-        DBI::dbWriteTable(conn = app_data$CON,
-                          name = "attendance",
-                          value = Write_Df,
-                          row.names = FALSE,
-                          overwrite = TRUE)
-        
-        showModal(
-          modalDialog(
-            title = "Success",
-            paste0(rv$target_ff_full_name, " has successfully checked in. You may now close this window."),
-            easyClose = TRUE
+        write_result <- DBI::dbExecute(app_data$CON,
+                       sql_command)
+        if(write_result == 1) {
+          showModal(
+            modalDialog(
+              title = "Success",
+              paste0(rvs$target_ff_full_name, " has successfully checked in. You may now close this window."),
+              easyClose = TRUE
+            )
           )
-        )
+        } else {
+          showModal(
+            modals$errorModal(paste("Check in write failed with write result equal to", write_result))
+          )
+        }
       })
       
       observeEvent(input$confirm_check_out, {
         removeModal()
-        # browser()
-        Write_Df <- app_data$Attendance |> 
-          dplyr::mutate(check_out = if_else(attendance_id == rv$attendance_id, as.POSIXct(Sys.time()), check_out))
-        rv$All <- Write_Df
+        browser()
+        sql_command <- paste0(
+          "UPDATE cfddb.attendance SET check_out = ",
+          "'", as.POSIXct(Sys.time()), "'",
+          "WHERE attendance_id = ", rvs$attendance_id)
         
-        DBI::dbWriteTable(app_data$CON,
-                          "attendance",
-                          value = Write_Df,
-                          row.names = FALSE,
-                          overwrite = TRUE)
+        write_result <- DBI::dbExecute(app_data$CON,
+                                       sql_command)
         
-        showModal(
-          modalDialog(
-            title = "Success",
-            paste0(rv$target_ff_full_name, " has successfully checked out. You may now close this window."),
-            easyClose = TRUE
+        if(write_result == 1) {
+          showModal(
+            modalDialog(
+              title = "Success",
+              paste0(rvs$target_ff_full_name, " has successfully checked out. You may now close this window."),
+              easyClose = TRUE
+            )
           )
-        )
+        } else {
+          showModal(
+            modals$errorModal(paste("Check out write failed with write result equal to", write_result))
+          )
+        }
       })
-      
     }
   )
 }
