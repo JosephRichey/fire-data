a06c57a678a6201e95761a3bba4042a9b4d09de6
JosephRichey <jwrichey@gmail.com>
2024-02-09 05:32:20 +0000

    Fully implemented auto checkout procedure.

---
+++ sql stuff/auto check out.sql
@@ -0,0 +1,33 @@
+USE cfddb;
+DROP PROCEDURE IF EXISTS auto_checkout;
+
+DELIMITER //
+CREATE PROCEDURE auto_checkout ()
+BEGIN
+
+-- This assumes there will only ever be one training per day, and this sp is ran every day.
+SELECT 
+    CONCAT(training_date, ' ', training_end_time) AS check_out
+INTO @check_out FROM
+    cfddb.training-- _TEST
+WHERE
+    training_id = (
+		SELECT training_id
+        FROM
+            cfddb.attendance-- _TEST
+        WHERE
+            check_out IS NULL);
+
+-- Update all null checkouts to checkout time.
+-- For CFD, this will run at 8:05 when training is always over.
+-- For dynamic use, create a python script to run this when a training finishes.
+UPDATE cfddb.attendance-- _TEST 
+SET 
+    check_out = @check_out,
+    auto_checkout = TRUE
+WHERE
+    check_out IS NULL;
+
+
+END//
+DELIMITER ;
--- sql stuff/schedule auto checkout.sql
+++ sql stuff/schedule auto checkout.sql
@@ -2,11 +2,11 @@ use cfddb;
 
 DELIMITER //
 
-CREATE EVENT weekly_auto_checkout
+ALTER EVENT weekly_auto_checkout
 ON SCHEDULE
     EVERY 1 DAY
-    STARTS '2024-02-03 20:06:00'
-    COMMENT 'Weekly auto checkout event on Tuesday at 20:06:00'
+    STARTS '2024-02-07 03:05:00'
+    COMMENT 'Weekly auto checkout event on Tuesday at 20:05:00'
 DO
     CALL auto_checkout();
 
