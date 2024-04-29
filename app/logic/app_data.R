9c1c9c3d4d0c2d5472b46b738466f36e221ae8d7
JosephRichey <jwrichey@gmail.com>
2024-04-29 07:21:08 +0000

    Updated data pull to convert all date and time fields to the appropriate data type.

--- app/logic/app_data.R
+++ app/logic/app_data.R
@@ -1,6 +1,7 @@
 box::use(
   DBI[...],
   tibble[...],
+  dplyr[...],
 )
 
 #' @export
@@ -13,14 +14,18 @@ CON <- dbConnect(RMySQL::MySQL(),
 
 #' @export
 Training <- dbGetQuery(CON,
-                       paste0("SELECT * FROM ", Sys.getenv("TRAINING_TABLE"),
-                              " WHERE training_delete IS NULL"))
+                       paste0("SELECT * FROM ", Sys.getenv("TRAINING_TABLE"))) |>
+  mutate(training_start_time = as.POSIXct(training_start_time),
+         training_end_time = as.POSIXct(training_end_time))
 
 #' @export
 Firefighter <- dbGetQuery(CON,
-                     paste0("SELECT * FROM ", Sys.getenv("FIREFIGHTER_TABLE"),
-                            " WHERE firefighter_deactive_date IS NULL"))
+                     paste0("SELECT * FROM ", Sys.getenv("FIREFIGHTER_TABLE"))) |>
+  mutate(firefighter_start_date = as.Date(firefighter_start_date),
+         firefighter_deactive_date = as.Date(firefighter_deactive_date))
 
 #' @export
 Attendance <- dbGetQuery(CON,
-                         paste0("SELECT * FROM ", Sys.getenv("ATTENDANCE_TABLE")))
+                         paste0("SELECT * FROM ", Sys.getenv("ATTENDANCE_TABLE"))) |>
+  mutate(check_in = as.POSIXct(check_in),
+         check_out = as.POSIXct(check_out))
