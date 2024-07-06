0c2b13401510b296a3ae62ed3fd7ba0d51a17fea
JosephRichey <jwrichey@gmail.com>
2024-07-06 21:30:56 +0000

    Initial setup of tests.

---
+++ tests/testthat/test-app_data.R
@@ -0,0 +1,23 @@
+box::use(
+  shiny[testServer],
+  testthat[...],
+  DBI[...],
+)
+
+box::use(
+  app/main[...],
+)
+
+test_that("Can connect to test database", {
+  CON <- dbConnect(RMySQL::MySQL(),
+                   dbname = "cfddb",
+                   host = Sys.getenv("CFDDB_HOST"),
+                   port = 3306,
+                   user = "admin",
+                   password = Sys.getenv("CFDDB_PASSWORD"))
+  result <- dbGetQuery(CON, paste0("SELECT * FROM ", Sys.getenv("TRAINING_TABLE"),
+                                   " WHERE training_delete IS NULL LIMIT 1"))
+  testthat::expect_is(result, "data.frame")
+
+
+})
