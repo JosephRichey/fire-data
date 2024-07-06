330d7a980e397c9417bbef5dfabe879ba2672294
JosephRichey <jwrichey@gmail.com>
2024-07-06 17:45:00 +0000

    Stored procedure now uses new database structure.

--- sql stuff/auto check out.sql
+++ sql stuff/auto check out.sql
@@ -5,30 +5,13 @@ DELIMITER //
 CREATE PROCEDURE sp_auto_checkout ()
 BEGIN
 
--- Find all records that require auto checkouts.
-
--- This currently assumes there will only ever be one training per day, and this sp is ran every day.
-SELECT 
-    CONCAT(training_date, ' ', training_end_time) AS check_out
-INTO @check_out FROM
-    cfddb.training-- _TEST
-WHERE
-    training_id = (
-		SELECT distinct training_id
-        FROM
-            cfddb.attendance-- _TEST
-        WHERE
-            check_out IS NULL);
-
--- Update all null checkouts to checkout time.
--- For CFD, this will run at 8:05 when training is always over.
--- For dynamic use, create a python script to run this when a training finishes.
-UPDATE cfddb.attendance-- _TEST 
-SET 
-    check_out = @check_out,
-    auto_checkout = TRUE
-WHERE
-    check_out IS NULL;
+-- Once a day, checkout everyone who is still checked in. Set end time as schedule end time of training.
+UPDATE cfddb.attendance atten
+LEFT JOIN cfddb.training train
+	ON atten.training_id = train.training_id
+SET check_out = training_end_time, 
+auto_checkout = TRUE
+WHERE CHECK_OUT IS NULL;
 
 
 END//
