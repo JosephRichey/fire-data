e6c69e93eac33cc142c2c6813a1b21c05ce00f0b
JosephRichey <jwrichey@gmail.com>
2024-07-08 00:25:58 +0000

    Created reset sp for testing data tables.

---
+++ sql stuff/schedule reset test tables.sql
@@ -0,0 +1,17 @@
+use cfddb;
+
+DROP EVENT IF EXISTS reset_test_tables
+
+DELIMITER //
+
+CREATE EVENT reset_test_tables
+ON SCHEDULE
+    EVERY 15 MINUTE
+    STARTS '2024-07-07 07:00:00'
+    COMMENT 'Reset test tables.'
+DO
+    CALL sp_reset_test_tables();
+
+//
+
+DELIMITER ;
