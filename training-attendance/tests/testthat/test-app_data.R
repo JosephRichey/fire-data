dc8202c5d42a7c83ecc6126530b2b389d8a80c51
JosephRichey <jwrichey@gmail.com>
2024-08-09 15:42:17 +0000

    Updated env variable name in test files.

--- training-attendance/tests/testthat/test-app_data.R
+++ training-attendance/tests/testthat/test-app_data.R
@@ -10,10 +10,10 @@ box::use(
 test_that("Can connect to test database", {
   CON <- dbConnect(RMySQL::MySQL(),
                    dbname = "cfddb",
-                   host = Sys.getenv("CFDDB_HOST"),
+                   host = Sys.getenv("DB_HOST"),
                    port = 3306,
                    user = "admin",
-                   password = Sys.getenv("CFDDB_PASSWORD"))
+                   password = Sys.getenv("DB_PASSWORD"))
   result <- dbGetQuery(CON, paste0("SELECT * FROM ", Sys.getenv("TRAINING_TABLE"),
                                    " WHERE training_delete IS NULL LIMIT 1"))
   testthat::expect_is(result, "data.frame")
