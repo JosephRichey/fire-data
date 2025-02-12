f081f0ef00b506acb790540ada62d122d3d8ac38
JosephRichey <jwrichey@gmail.com>
2025-02-12 21:16:24 +0000

    Initial work on new layout for data portal.

--- data-portal/app/main.R
+++ data-portal/app/main.R
@@ -6,6 +6,8 @@ box::use(
   bslib[...],
   DT[...],
   DBI[dbDisconnect],
+  shiny.router[...],
+  fontawesome[...],
 )
 
 box::use(
@@ -15,68 +17,120 @@ box::use(
   logic/app_data,
 )
 
+menu <- tags$ul(
+  class = "nav-icons",
+  tags$li(a(href = route_link("training"), fa("book-open", title = "Training"))),
+  tags$li(a(href = route_link("incident"), fa("calendar-check", title = "Incident"))),
+  tags$li(a(href = route_link("equipment"), fa("fire-extinguisher", title = "Equipment Management"))),
+  tags$li(a(href = route_link("reports"), fa("chart-pie", title = "reports"))),
+  tags$li(a(href = route_link("roster"), fa("user", title = "Roster"))),
+  tags$li(a(href = route_link("messaging"), fa("envelope", title = "Messaging"))),
+  tags$li(a(href = route_link("settings"), fa("cogs", title = "Settings")))
+)
+
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  page_navbar(
+  page_fillable(
     title = paste0(Sys.getenv('FD'), " - Data Portal"),
     theme = bs_theme(version = 5,
-                     success = "#87292b",
-                     primary = '#ae3537',
+                     primary = "#87292b",
+                     secondary = '#a05050',
+                     success = "#2b8764",
+                     info = "#377eb8",
+                     warning = "#D76F33",
+                     danger = "#640064",
+                     light = "#565656",
                      bootswatch = "darkly"),
-    nav_panel(title = "Training",
-              layout_sidebar(
-                sidebar = sidebar(
-                  width = 400,
-                  open = "desktop",
-                  training$UI(ns('training'))
-                ),
-                training$Output(ns('training'))
-              )
-    ),
-
-    nav_panel(title = "Manage Roster",
-              layout_sidebar(
-                sidebar = sidebar(
-                  width = 400,
-                  open = "desktop",
-                  roster$UI(ns('roster')),
-                ),
-
-                roster$Output(ns('roster'))
-
-              )
-
-    ),
-
-    nav_panel(title = "Training Summary",
-              navset_pill(
-                nav_panel(title = "Individual",
-                          layout_sidebar(
-                            sidebar = sidebar(
-                              title = "Set Filters",
-                              summary$UI(ns('ind_summary'), "Individual")
+    # Sidebar with icons
+    div(class = "nav-icons", menu),
+    div(class = "main-content",
+        router_ui(
+          route("training",
+                layout_sidebar(
+                              sidebar = sidebar(
+                                width = 400,
+                                open = "desktop",
+                                training$UI(ns('training'))
                               ),
-                            summary$Output(ns('ind_summary'), "Individual")
+                              training$Output(ns('training'))
                             )
-                          ),
+          ),
+          route("incident",
+                page_navbar(
+                  title = "Incident",
+                  sidebar = sidebar(
+                    numericInput("input2", "Input 2", value = 0)
+                  )
+                )),
+          route("equipment",
+                page_navbar(
+                  title = "Equipment Management",
+                  sidebar = sidebar(
+                    textInput("input3", "Input 3", value = "")
+                  )
+                )),
+          route("reports",
+            navset_pill(
+              nav_panel(
+                title = "Individual",
+                layout_sidebar(
+                  sidebar = sidebar(
+                    title = "Set Filters",
+                    summary$UI(ns('ind_summary'), "Individual")
+                    ),
+                  summary$Output(ns('ind_summary'), "Individual")
+                  )
+                ),
 
-                nav_panel(title = "Department",
-                          layout_sidebar(
-                            sidebar = sidebar(
-                              title = "Set Filters",
-                              summary$UI(ns('dep_summary'), "Department")
-                              ),
-                            summary$Output(ns('dep_summary'), "Department")
-                            )
+              nav_panel(title = "Department",
+                        layout_sidebar(
+                          sidebar = sidebar(
+                            title = "Set Filters",
+                            summary$UI(
+                              ns('dep_summary'),
+                              "Department")
+                            ),
+                          summary$Output(
+                            ns('dep_summary'),
+                            "Department")
                           )
+                        )
+              )
+            ),
+          route("roster",
+                layout_sidebar(
+                              sidebar = sidebar(
+                                width = 400,
+                                open = "desktop",
+                                roster$UI(ns('roster')),
+                              ),
+
+                              roster$Output(ns('roster'))
+
+                            )
+          ),
+          route("messaging",
+                page_navbar(
+                  title = "Messaging",
+                  sidebar = sidebar(
+                    dateInput("input6", "Input 6", value = Sys.Date())
+                  )
+                )),
+          route("settings",
+                page_navbar(
+                  title = "Settings",
+                  sidebar = sidebar(
+                    dateInput("input7", "Input 7", value = Sys.Date())
+                  )
                 )
+              )
+          ),
 
-    ),
     nav_spacer(),
-    helpText("v0.3.0 © CC BY-NC-SA 2024 Joseph Richey")
-
+    helpText("dev-1.0.0 © CC BY-NC-SA 2024 Joseph Richey")
+    )
   )
 }
 
@@ -94,6 +148,8 @@ server <- function(id) {
     summary$Server('ind_summary', 'Individual')
 
     summary$Server('dep_summary', 'Department')
+
+    router_server()
 
     # Disconnect from the database on app close
     session$onSessionEnded(function() {
